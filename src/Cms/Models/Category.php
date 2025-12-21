e7054a42bf34803c58ae5b8b57b2002426ae8858
Lee Jones <lee@dragonflyrg.com>
2025-12-21 16:14:11 -0600

    updates

--- src/Cms/Models/Category.php
+++ src/Cms/Models/Category.php
@@ -181,13 +181,20 @@ class Category extends Model
 	 */
 	public function toArray(): array
 	{
-		return [
-			'id' => $this->_id,
+		$data = [
 			'name' => $this->_name,
 			'slug' => $this->_slug,
 			'description' => $this->_description,
 			'created_at' => $this->_createdAt?->format( 'Y-m-d H:i:s' ),
 			'updated_at' => $this->_updatedAt?->format( 'Y-m-d H:i:s' ),
 		];
+
+		// Only include id if it's set (not null) to avoid PostgreSQL NOT NULL constraint errors
+		if( $this->_id !== null )
+		{
+			$data['id'] = $this->_id;
+		}
+
+		return $data;
 	}
 }
