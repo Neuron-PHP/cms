08c8ad54494a7e82a7feadcf75dd84d0f9d2567c
Lee Jones <lee@dragonflyrg.com>
2026-01-14 17:31:09 -0600

    fixes 404 page.

--- resources/views/http_codes/404.php
+++ resources/views/http_codes/404.php
@@ -1,7 +1,9 @@
 <div class="row">
 	<div class="col">
-		<h1 class="centered text-secondary">404.</h1>
+		<h1 class="centered text-secondary">404</h1>
 	</div>
+</div>
+<div class="row">
 	<div class="col">
 		<h2 class="centered">The page you are looking for does not exist.</h2>
 	</div>
